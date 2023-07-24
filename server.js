a1226c02f8b490b2704824e838e2b9fb7857ecd7
Alison Colao <alison.siwek@gmail.com>
2023-07-23 20:39:12 -0700

    updated gitignore

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const session = require('express-session')
 const exphbs = require('express-handlebars')
 
 const app = express()
+const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/index')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
